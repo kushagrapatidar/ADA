d5615e034cc61f345f2fd1251045d0cdf9df6e5c
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-01-18 12:01:18 +0530

    main

--- Practical/linear_search.py
+++ Practical/linear_search.py
@@ -1,12 +1,20 @@
-def linearsearch(n, idx=list(), arr=None):
+def linearsearch(n):
     from random import randint
-    if arr==None:
-        arr=[randint(0,x+1) for x in range(1,101)]
-        print(f'{arr}\n Length: {len(arr)} \n')
-    
-    for _ in arr:
-        if _==n:
-            idx.append(arr.index(_)+1)
-            idx=linearsearch(n, idx, arr[idx[-1]:len(arr)])
+    arr=[randint(0,x+1) for x in range(1,101)]
+    print(f'{arr}\n Length: {len(arr)} \n')
+    if n in arr:
+        idx=arr.index(n)+1
+        print(f"{n} found in the list: Index- {idx}")
+    else:
+        print(f'{n} not found in the list')
+        
+def linearsearch2(n):
+    from random import randint
+    arr=[randint(0,x+1) for x in range(1,101)]
+    print(f'{arr}\n Length: {len(arr)} \n')
+    idx=list()
+    for _ in range(len(arr)):
+        if arr[_]==n:
+            idx.append(_)
     
     return(idx)
--- Practical/test.py
+++ Practical/test.py
@@ -1,17 +1,15 @@
 import datetime, os
 os.system('cls')
 
-a = datetime.datetime.now()
-from linear_search import linearsearch
-n=int(input('Enter the number: '))
-idx_lst=linearsearch(n)
-if idx_lst==None:
-    print(f'{n} not found in the list')
-else:
-    print(f"{n} found in the list: Frequency- {len(idx_lst)}\nIndex- {idx_lst}")
-b = datetime.datetime.now()
-print(b-a)
+# Linear Search -1
+# a = datetime.datetime.now()
+# from linear_search import linearsearch
+# n=int(input('Enter the number: '))
+# linearsearch(n)
+# b = datetime.datetime.now()
+# print(b-a)
 
+# Insertion Sort
 # a = datetime.datetime.now()
 # from insertion_sort import insertionsort
 # arr=input('Enter the array elements with spaces: ').split()
@@ -20,3 +18,15 @@ print(b-a)
 # print(arr)
 # b = datetime.datetime.now()
 # print(b-a)
+
+#Linear Search -2
+a = datetime.datetime.now()
+from linear_search import linearsearch2
+n=int(input('Enter the number: '))
+idx_lst=linearsearch2(n)
+if idx_lst==None:
+    print(f'{n} not found in the list')
+else:
+    print(f"{n} found in the list: Frequency- {len(idx_lst)}\nIndex- {idx_lst}")
+b = datetime.datetime.now()
+print(b-a)
