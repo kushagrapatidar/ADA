599d8941eec692f80ce721072f3585283cdc1c48
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-01-18 11:03:31 +0530

    main

--- Practical/test.py
+++ Practical/test.py
@@ -1,7 +1,10 @@
+from os import system
+system('cls')
 from linear_search import linearsearch
 n=int(input('Enter the number: '))
 linearsearch(n)
 
+system('cls')
 # from insertion_sort import insertionsort
 # arr=input('Enter the array elements with spaces: ').split()
 # arr=list(map(int, arr))
