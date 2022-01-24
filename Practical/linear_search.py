b23704c73623b547fd4f5c60bea142f58d0e3c5a
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-01-24 21:53:51 +0530

    main

--- Practical/linear_search.py
+++ Practical/linear_search.py
@@ -1,8 +1,11 @@
+import os
+os.system('cls')
+
 #Que-01
 def linearsearch(n):
     from random import randint
     arr=[randint(0,x+1) for x in range(1,101)]
-    #print(f'{arr}\n Length: {len(arr)} \n')
+    print(f'{arr}\n')# Length: {len(arr)} \n')
     if n in arr:
         idx=arr.index(n)+1
         print(f"{n} found in the list: Index- {idx}")
@@ -13,7 +16,7 @@ def linearsearch(n):
 def linearsearch2(n):
     from random import randint
     arr=[randint(0,x+1) for x in range(1,101)]
-    #print(f'{arr}\n Length: {len(arr)} \n')
+    print(f'{arr}\n')# Length: {len(arr)} \n')
     idx=list()
     for _ in range(len(arr)):
         if arr[_]==n:
--- Practical/test.py
+++ Practical/test.py
@@ -2,28 +2,28 @@ import datetime, os
 os.system('cls')
 
 # Linear Search -1
-# a = datetime.datetime.now()
 # from linear_search import linearsearch
 # n=int(input('Enter the number: '))
 # linearsearch(n)
-# b = datetime.datetime.now()
-# print(b-a)
+
+# #Linear Search - 2
+# from linear_search import linearsearch2
+# n=int(input('Enter the number: '))
+# idx_lst=linearsearch2(n)
+# if idx_lst==None:
+#     print(f'{n} not found in the list')
+# else:
+#     print(f"{n} found in the list:\nIndex- {idx_lst}")
+
+#Linear Search - 3
+from linear_search import find_freq
+n3=int(input('Enter the number: '))
+frequency=find_freq(n3)
+print(f'Frequency of occurrence of {n3} in the list: {frequency}')
 
 # Insertion Sort
-# a = datetime.datetime.now()
 # from insertion_sort import insertionsort
 # arr=input('Enter the array elements with spaces: ').split()
 # arr=list(map(int, arr))
 # insertionsort(arr)
 # print(arr)
-# b = datetime.datetime.now()
-# print(b-a)
-
-#Linear Search -2
-from linear_search import linearsearch2, find_freq
-n=int(input('Enter the number: '))
-idx_lst=linearsearch2(n)
-if idx_lst==None:
-    print(f'{n} not found in the list')
-else:
-    print(f"{n} found in the list:\nIndex- {idx_lst}")
