98bbbbc5a5ce117efdccc5d0021deec1324e4a18
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-01-25 10:39:50 +0530

    main

--- Practical/insertion_sort.py
+++ Practical/insertion_sort.py
@@ -1,4 +1,15 @@
-def insertionsort(arr):
+def insertionsort_asc(arr):
+    l=len(arr)
+    print(arr)
+    for i in range(1,l):
+        store=arr[i]
+        j=i-1
+        while j>=0 and store<arr[j]:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=store
+
+def insertionsort_desc(arr):
     l=len(arr)
     print(arr)
     for i in range(1,l):
--- Practical/test.py
+++ Practical/test.py
@@ -1,4 +1,5 @@
 import datetime, os
+from Practical.insertion_sort import insertionsort_asc, insertionsort_desc
 os.system('cls')
 
 # Linear Search -1
@@ -15,15 +16,15 @@ os.system('cls')
 # else:
 #     print(f"{n} found in the list:\nIndex- {idx_lst}")
 
-#Linear Search - 3
-from linear_search import find_freq
-n3=int(input('Enter the number: '))
-frequency=find_freq(n3)
-print(f'Frequency of occurrence of {n3} in the list: {frequency}')
+# #Linear Search - 3
+# from linear_search import find_freq
+# n3=int(input('Enter the number: '))
+# frequency=find_freq(n3)
+# print(f'Frequency of occurrence of {n3} in the list: {frequency}')
 
-# Insertion Sort
-# from insertion_sort import insertionsort
-# arr=input('Enter the array elements with spaces: ').split()
-# arr=list(map(int, arr))
-# insertionsort(arr)
-# print(arr)
+#Insertion Sort
+from insertion_sort import insertionsort_desc
+arr=input('Enter the array elements with spaces: ').split()
+arr=list(map(int, arr))
+insertionsort_desc(arr)
+print(arr,'\n'*2)
