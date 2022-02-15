31e762f19d38cd3b39148b9198a519e55369c323
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-15 11:53:29 +0530

    main

--- Practical/heap_sort.py
+++ Practical/heap_sort.py
@@ -1,4 +1,4 @@
-def heapsort(arr):
+def max_heapsort(arr):
     n=len(arr)-1
     
     for j in range(2):
@@ -15,19 +15,46 @@ def heapsort(arr):
             
     return arr
 
-def call_heapsort(arr):
+def min_heapsort(arr):
+    n=len(arr)-1
+    
+    for j in range(2):
+        i=n
+        
+        while i>0:
+            
+            if arr[i]<arr[i-1]:
+                arr[i],arr[i-1]=arr[i-1],arr[i]
+            
+            if arr[i-1]<arr[(i-1)//2]:
+                arr[i-1],arr[(i-1)//2]=arr[(i-1)//2],arr[i-1]
+            i-=2
+            
+    return arr
+
+def call_min_heapsort(arr):
     i=len(arr)
     
     while i>0:
-        arr[:i]=heapsort(arr[:i])
+        arr[:i]=min_heapsort(arr[:i])
         i-=1
+    return arr
+
+def call_max_heapsort(arr):
+    i=len(arr)
+    
+    while i>0:
+        arr[:i]=max_heapsort(arr[:i])
+        i-=1
+    return list(reversed(arr))
 
 if __name__=="__main__":
     arr=list(map(int, input('Enter the array elements with spaces: ').split()))
     print('Initial Array:\n',arr,"\n")
-    call_heapsort(arr)
-
-    print('Sorted Array:\n',arr)
+    arr_max=call_max_heapsort(arr.copy())
+    print('Sorted Array by Max Heap:\n',arr_max)
+    arr_min=call_min_heapsort(arr.copy())
+    print('Sorted Array by Min Heap:\n',arr_min)
 
 # Rough Work
     # Corresponding parent and first child node
