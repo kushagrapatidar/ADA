639bdf102e8481f7975e98417c5d36d97e5ae06d
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-15 10:48:06 +0530

    main

--- Practical/merge_sort.py
+++ Practical/merge_sort.py
@@ -1,3 +1,25 @@
+def merge(arr,L,R):
+    i=j=k=0
+    while i<len(L) and j<len(R):
+        if L[i]<R[j]:  
+            arr[k]=L[i]
+            i+=1
+
+        else:
+            arr[k]=R[j]
+            j+=1
+        k+=1
+    
+    while i<len(L):
+        arr[k]=L[i]
+        i+=1
+        k+=1
+    
+    while j<len(R):
+        arr[k]=R[j]
+        j+=1
+        k+=1
+            
 def mergesort(arr):
     if len(arr)>1: 
         mid=len(arr)//2
@@ -8,28 +30,8 @@ def mergesort(arr):
         mergesort(L)
         
         mergesort(R)
-        print(R)
         
-        i=j=k=0
-        while i<len(L) and j<len(R):
-            if L[i]<R[j]:  
-                arr[k]=L[i]
-                i+=1
-
-            else:
-                arr[k]=R[j]
-                j+=1
-            k+=1
-        
-        while i<len(L):
-            arr[k]=L[i]
-            i+=1
-            k+=1
-        
-        while j<len(R):
-            arr[k]=R[j]
-            j+=1
-            k+=1
+        merge(arr,L,R)
 
 if __name__=="__main__":
     arr=list(map(int, input('Enter the array elements with spaces: ').split()))
