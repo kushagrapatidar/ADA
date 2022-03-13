f322c05947c36370cf898be1ff6de90cbcf03d3b
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-03-13 13:59:17 +0530

    main

--- Practical/LongestCommonSubsequence.py.py
+++ Practical/LongestCommonSubsequence.py.py
@@ -3,7 +3,6 @@ S2="BDGHCEJ"
 n=len(S1)
 m=len(S2)
 
-# LCS=[[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0]]
 LCS=[]
 
 for i in range(m+1):
@@ -11,8 +10,6 @@ for i in range(m+1):
     for j in range(n+1):
         lst.append(0)
     LCS.append(lst)
-# [print(row) for row in LCS]
-# print("\n")
 
 for i in range(1,m+1):
     for j in range(1,n+1):
@@ -20,5 +17,17 @@ for i in range(1,m+1):
             LCS[i][j]=LCS[i-1][j-1]+1
         else:
             LCS[i][j]=max(LCS[i-1][j],LCS[i][j-1])
-[print(row) for row in LCS]
 
+i=m
+j=n
+Soln=""
+while j>=0 and i>0:
+    if LCS[i][j]==LCS[i-1][j]:
+        i-=1
+    else:
+        Soln=S2[i-1]+Soln
+        i-=1
+        j-=1
+print("String 1: "+S1)
+print("String 2: "+S2)
+print("Lowest Common Subsequence: "+Soln)
