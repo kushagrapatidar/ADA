44693248302cdc4b3f0748530489da3a1039d7d2
Kushagra Patidar <triponn.team@gmail.com>
2022-03-15 08:44:45 +0530

    Update matrixmult.py

--- Practical/matrixmult.py
+++ Practical/matrixmult.py
@@ -1,15 +1,51 @@
-import sys
-
-def matrixchainorder(p, i, j):
-    if i == j:
-        return 0
-    _min = sys.maxsize
-    for k in range(i, j):
-        val = (matrixchainorder(p, i, k)+matrixchainorder(p, k + 1, j)+p[i-1]*p[k]*p[j])
-        if val < _min:
-            _min = val
-    return _min
-
-arr = [5, 4, 6, 2, 7]
-n = len(arr)
-print("Minimum number of multiplications is ",matrixchainorder(arr, 1, n-1))
+def matrix_product(p):
+    length = len(p)
+    m = [[-1]*length for _ in range(length)]
+    s = [[-1]*length for _ in range(length)]
+ 
+    matrix_product_helper(p, 1, length - 1, m, s)
+ 
+    return m, s
+def matrix_product_helper(p, start, end, m, s):
+    if m[start][end] >= 0:
+        return m[start][end]
+ 
+    if start == end:
+        q = 0
+    else:
+        q = float('inf')
+        for k in range(start, end):
+            temp = matrix_product_helper(p, start, k, m, s)+ matrix_product_helper(p, k + 1, end, m, s)+ p[start - 1]*p[k]*p[end]
+            if q > temp:
+                q = temp
+                s[start][end] = k
+ 
+    m[start][end] = q
+    return q
+ 
+ 
+def print_parenthesization(s, start, end):
+    if start == end:
+        print(' A[{}] '.format(start), end='')
+        return
+ 
+    k = s[start][end]
+ 
+    print('(', end='')
+    print_parenthesization(s, start, k)
+    print_parenthesization(s, k + 1, end)
+    print(')', end='')
+ 
+ 
+n = int(input('Enter number of matrices: '))
+p = []
+for i in range(n):
+    temp = int(input('Enter number of rows in matrix {}: '.format(i + 1)))
+    p.append(temp)
+temp = int(input('Enter number of columns in matrix {}: '.format(n)))
+p.append(temp)
+ 
+m, s = matrix_product(p)
+print('The number of scalar multiplications needed:', m[1][n])
+print('Optimal parenthesizing: ', end='')
+print_parenthesization(s, 1, n)
